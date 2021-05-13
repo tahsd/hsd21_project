530128e6573a73d7e2aed249999b1e10fc063c5d
tahsd <63864485+tahsd@users.noreply.github.com>
2021-05-13 19:53:34 +0900

    modify benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# Acc: 0.97
-sudo python eval.py --num_test_images 100 --m_size 64 --v_size 64 --network mlp --run_type fpga
-echo -e '\n=> Accuracy should be 0.97\n'
+# You should compare the difference betwween the results of cpu vs fpga version.
+# Analyze the results and include it in your report.
 
-# Acc: 1.0
+# Conv Lowering(CPU) - Acc: 1.0
+sudo python eval.py --num_test_images 100 --v_size 8 --network cnn --run_type cpu
+echo -e '\n=> Accuracy should be 1.0\n'
+
+# Conv Lowering(FPGA) - Acc: 1.0
 sudo python eval.py --num_test_images 100 --v_size 8 --network cnn --run_type fpga
 echo -e '\n=> Accuracy should be 1.0'
